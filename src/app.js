9ba9bd566cbc1442fe14aec2fb8a563a559aea0d
Andy Zhong <andyjzhong@gmail.com>
2018-06-05 15:33:33 +0000

    Adding necessary files and dependencies

---
+++ src/app.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h3>My React App Title</h3>,
+  document.getElementById('react-container')
+);
---
+++ src/test.js
@@ -0,0 +1,6 @@
+class myclass {
+  constructor(name) {this.name = name;}
+  display() {console.log(this.name);}
+}
+
+new myclass('myclass').display();
