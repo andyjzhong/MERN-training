ae6b2c71237e057ae4a42b8810758f4f63da4a68
Andy Zhong <andyjzhong@gmail.com>
2018-06-05 18:12:22 +0000

    Added very basic components with props

--- src/app.js
+++ src/app.js
@@ -1,4 +1,43 @@
-ReactDOM.render(
-  <h3>My React App Title</h3>,
-  document.getElementById('react-container')
-);
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body author={ author } />
+      <Footer text={ footerText } />
+    </div>
+  )
+}
+
+function Header() {
+  const title = "Here is the title you told me to make"
+  return (
+    <h3>{title}</h3>
+  )
+}
+
+function Body(props) {
+  return (
+    <div>
+      <p>{props.author.name}</p>
+      <p>{props.author.phone}</p>
+      <p>{props.author.email}</p>
+    </div>
+  )
+}
+
+var footerText = "Footer Text Whaaaaaat??"
+var author = {
+  name: "John Doe",
+  phone: "111-111-1111",
+  email: "email@email.com"
+}
+
+function Footer(props) {
+  return (
+    <div>
+      <h4>{props.text}</h4>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('react-container'))
